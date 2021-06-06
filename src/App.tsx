8c5a2379fc86e66a18cfd13167ed731661845239
wangzc <1141840739@qq.com>
2021-06-06 19:00:19 +0800

    初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
+import Button, {ButtonType, ButtonSize} from './components/Button/button';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <div>
+          <Button  size='lg' btnType='danger' >button</Button>
+
+          <Button  size='sm'>button</Button>
+
+          <Button  btnType='danger' disabled>button</Button>
+
+          <Button  btnType='link'>button</Button>
+
+          <Button  btnType='primary'>button</Button>
+
+          <Button  btnType='link' disabled>button</Button>
+        </div>
       </header>
     </div>
   );
